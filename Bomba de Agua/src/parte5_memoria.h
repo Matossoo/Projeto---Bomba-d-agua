d0792104b5104dd0c74e1f3d4fdb4db5af7a5c16
GABRIEL BAPTISTA MARTINS <gabriel_martins28@estudante.sesisenai.org.br>
2026-03-24 18:28:24 -0300

    Atualização para agora esvaziar o reservatorio

---
+++ Bomba de Agua/src/parte5_memoria.h
@@ -0,0 +1,16 @@
+ ALTERADO: O sistema continua salvando na memória mesmo se estiver esvaziando com o LED apagado
+  bool sistemaAtivo = (enchendo || esvaziando);
+  
+  if (sistemaAtivo && (millis() - tempoUltimoSave >= 500)) {
+    EEPROM.put(addrConsumo, consumo);
+    EEPROM.put(addrNivel, nivelAlvo);
+    tempoUltimoSave = millis();
+  }
+  
+  static bool estadoAnteriorSistema = false;
+  if (estadoAnteriorSistema == true && sistemaAtivo == false) {
+    EEPROM.put(addrConsumo, consumo);
+    EEPROM.put(addrNivel, nivelAlvo);
+    tempoUltimoSave = millis(); 
+  }
+  estadoAnteriorSistema = sistemaAtivo;
